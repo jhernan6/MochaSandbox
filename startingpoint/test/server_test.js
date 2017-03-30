f853fa2020b29cc7f1432e451a7fd0f57611a89d
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-03-30 01:06:34 -0700

    Added initial describe to test file

--- startingpoint/test/server_test.js
+++ startingpoint/test/server_test.js
@@ -1,13 +1,15 @@
 var expect = require("chai").expect;
 var request = require("request");
 
-describe("RGB to Hex conversion", function(){
+describe ("Color Code Converter API", function(){
+	describe("RGB to Hex conversion", function(){
 
-	it("return status 200", function(){
+		it("return status 200", function(){
 
-	});
+		});
 
-	it("return the color in hex", function(){
+		it("return the color in hex", function(){
 
+		});
 	});
 });
