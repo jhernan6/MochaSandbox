c9ed949697e6826aa0147b84c527fe298362bbc4
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-03-30 01:02:44 -0700

    Started to created response test

--- startingpoint/test/server_test.js
+++ startingpoint/test/server_test.js
@@ -1,2 +1,13 @@
 var expect = require("chai").expect;
 var request = require("request");
+
+describe("RGB to Hex conversion", function(){
+
+	it("return status 200", function(){
+
+	});
+
+	it("return the color in hex", function(){
+
+	});
+});
