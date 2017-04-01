eed7f1776a55d10be0c70312146b5c8724c6a5a7
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-01 00:11:23 -0700

    Updated test script and create a simple assert test

---
+++ take2/test/test1.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+
+
+//First example that is shown in the Getting Started section
+
+describe("Array", function() {
+
+	describe("#indexOf()", function() {
+
+		//Simple test to verify that there is no value in the index
+		it("should return -1 when out of index bound", function(){
+
+			assert.equal(-1, [1,2,3].indexOf(4));
+
+		});
+	});
+});
