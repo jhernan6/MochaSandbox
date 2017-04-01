00a64f3a25024e2b48013454dc80273f5e09e0c8
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-03-31 21:48:45 -0700

    Fixed issue with done in server tests

--- startingpoint/test/server_test.js
+++ startingpoint/test/server_test.js
@@ -2,20 +2,24 @@ var expect = require("chai").expect;
 var request = require("request");
 
 describe ("Color Code Converter API", function(){
-	
+
 	describe("RGB to Hex conversion white example", function(){
 
 		var url =  "http://localhost:3000/rgbToHex?red=255&green=255&blue=255";
 
-		it("return status 200", function(){
+		it("return status 200", function(done){
+
 			request(url, function(error, response, body){
+
 				expect(response.statusCode).to.equal(200);
 				done();
 			});
 		});
 
-		it("return the color in hex", function(){
+		it("return the color in hex", function(done){
+			
 			request(url, function(error, response, body){
+				
 				expect(body).to.equal("ffffff");
 				done();
 			});
@@ -26,15 +30,18 @@ describe ("Color Code Converter API", function(){
 
 		var url = "http://localhost:300/hextToRgb?hex=00ff00";
 
-		it("return status 200", function(){
+		it("return status 200", function(done){
+			
 			request(url, function(error, response, body){
+				
 				expect(response.statusCode).to.equal(200);
 				done();
 			});
 		});
 
-		it("return the color in RGB", function(){
+		it("return the color in RGB", function(done){
 			request(url, function(error, response, body){
+				
 				expect(body).to.equal("[0, 255, 0]");
 				done();
 			});
