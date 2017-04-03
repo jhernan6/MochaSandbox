76080c88de20ac67c1a3964ece6e42697266d1f7
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-03 00:44:16 -0700

    Added chance to test dir and created simple assert tests

--- take2/test/test2.js
+++ take2/test/test2.js
@@ -1,16 +1,31 @@
 var assert = require('assert');
+var Chance = require('chance');
+
+var chance = new Chance();
 
 describe("Simple assertion tests", function(){
 
 	describe("Tests that will be using simple assert",function(){
 
-		var bool = true;
-		var prime = 3;
-		var str = "This is a string";
+		var bool = chance.bool();
+		var randnum = chance.integer({min: 1, max: 100});
+		var str = chance.string();
 
-		it('assert on a boolean value', function(){
+		console.log(str);
 
-			console.log(assert(true));
+		it('assert on a random boolean value to be true', function(){
+
+			assert(bool === false, "The random generated boolean was false");
+		});
+
+		it('assert on a random number to see if it is even', function(){
+			
+			assert((randnum % 2) === 0, "The random generated number is not even");
+		});
+
+		it("assert on a random string to be larger than 10 chars", function(){
+
+			assert(str.length > 10, "The string is larger than 15 chars");
 		});
 
 	});
