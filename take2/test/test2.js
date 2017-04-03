64942d2c9ae444340e082d69888bed4466d96ade
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-02 23:51:19 -0700

    Simple test file to use NodeJS assertion class

---
+++ take2/test/test2.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+describe("Simple assertion tests", function(){
+
+	const bool = true;
+
+	const prime = 3;
+
+	it("Simple assertion of boolean values",function(){
+
+		assert(bool, true);
+	});
+
+
+
+})
