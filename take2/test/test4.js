9dab0d045b029b992462caed1a4dc61d4ca58c3a
Jose Hernandez <jhernan6@alumni.uci.edu>
2017-04-05 23:08:45 -0700

    Created new test file

---
+++ take2/test/test4.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+var Chance = require('chance');
+var chance = new Chance();
+
+describe("Testfile #4. All the test below will be using strict equal", function(){
+
+});
